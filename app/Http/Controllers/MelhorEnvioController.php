ca6682448e44caaaad9b8eace1e709e3d4d8551d
BrunoRangel30 <rangel.bruno28@gmail.com>
2021-08-13 17:06:29 -0300

    teste api_V4

--- app/Http/Controllers/MelhorEnvioController.php
+++ app/Http/Controllers/MelhorEnvioController.php
@@ -9,17 +9,27 @@ class MelhorEnvioController extends Controller
     public function autorizacaoAPI(){
 
         $client_id = config('app.client_id');
-        $service = $service;
+       // $service = purchases-read ;
         $url = config('app.urlME');
         $client_secret = config('app.client_secret');
         $redirect_uri = config('app.redirect_uri');
-        $urlCurl = "$url/oauth/authorize?client_id=$client_id&redirect_uri=$redirect_uri&response_type=code&scope=$service";
+        $urlCurl = "$url/oauth/authorize?client_id=$client_id&redirect_uri=$redirect_uri&response_type=code&scope= shipping-tracking shipping-print shipping-preview shipping-generate shipping-checkout cart-read cart-write orders-read shipping-calculate shipping-cancel";
         return redirect($urlCurl);
         
     }
     public function callback(){
         //adquiri um token
-        return  json_decode($_GET["code"]);
+        if(isset($_GET["code"])){
+            return [
+                'codigo' => json_decode($_GET["code"]),
+                'status'   => "OK",
+            ];
+        }else{
+            return [
+                'codigo' => null,
+                'status'   => "codigo não retornado",
+            ];
+        }
     
     }
 }
